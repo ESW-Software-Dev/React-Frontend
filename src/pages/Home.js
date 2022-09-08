5ac2aa99a9e888c4efe35028d5f08654e97ed3ba
Charlie Mueller <cmm478@cornell.edu>
2022-09-08 19:37:20 -0400

    Navbar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Logo from "../components/Logo.js";
+import { Navbar } from 'react-bootstrap';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <Logo />
       <h2>this is the home page</h2>
     </div>
